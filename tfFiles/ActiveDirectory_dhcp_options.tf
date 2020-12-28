6e47600805bfe9b0257273043b25cb9df8552505
root <root@nat.amit.com>
2020-12-28 19:03:41 +0000

    deleted:    tfFiles/.Variables.tf.swp 	modified:   tfFiles/ActiveDirectory_dhcp_options.tf 	modified:   tfFiles/output.tf

--- tfFiles/ActiveDirectory_dhcp_options.tf
+++ tfFiles/ActiveDirectory_dhcp_options.tf
@@ -1,9 +1,10 @@
 resource "aws_vpc_dhcp_options" "DhcpOptionforAD" {
-  domain_name          = var.DOMAINNAME
-  domain_name_servers=[join(",",aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses,[var.DNSSERVERFORVPC])]
-  ntp_servers          = aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses
-  netbios_name_servers = aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses
-   tags = {
+ domain_name          = var.DOMAINNAME
+ #domain_name_servers=[join(",",aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses,[var.DNSSERVERFORVPC])]
+ domain_name_servers=[join(",",[var.DNSSERVERFORVPC],aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses)]
+ ntp_servers          = aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses
+ netbios_name_servers = aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses
+  tags = {
                   Name = "DhcpOptionforAD"
             DomainName = var.DOMAINNAME
         }
--- tfFiles/output.tf
+++ tfFiles/output.tf
@@ -1,3 +1,15 @@
-output "aws_ips" {
+output "aws_ActiveDirectory_IP" {
   value = aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses
+}
+output "aws_var_IP" {
+  value = [var.DNSSERVERFORVPC]
+}
+output "aws_ActiveDirectory_var_IP"{
+ value = [join(",",aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses,[var.DNSSERVERFORVPC])]
+}
+output "count_number_of_elements"{
+  value = length(aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses)
+}
+output "aws_var_ActiveDirectory_IP"{
+ value = [join(",",[var.DNSSERVERFORVPC],aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses)]
 }
