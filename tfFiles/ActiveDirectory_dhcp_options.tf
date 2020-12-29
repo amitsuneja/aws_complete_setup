f4fb5947243f128bb518542917a974c3e53529f2
root <root@nat.amit.com>
2020-12-29 21:52:53 +0000

    modified:   tfFiles/ActiveDirectory_dhcp_options.tf 	modified:   tfFiles/dynamicFiles/aws_hosts 	modified:   tfFiles/playBooks/BasionHost.yml

--- tfFiles/ActiveDirectory_dhcp_options.tf
+++ tfFiles/ActiveDirectory_dhcp_options.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_dhcp_options" "DhcpOptionforAD" {
  domain_name          = var.DOMAINNAME
- #domain_name_servers=[join(",",aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses,[var.DNSSERVERFORVPC])]
- domain_name_servers=[join(",",[var.DNSSERVERFORVPC],aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses)]
+ domain_name_servers=[join(",",aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses,[var.DNSSERVERFORVPC])]
+ #domain_name_servers=[join(",",[var.DNSSERVERFORVPC],aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses)]
  ntp_servers          = aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses
  netbios_name_servers = aws_directory_service_directory.MyActiveDirectory.dns_ip_addresses
   tags = {
